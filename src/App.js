5463211eb47e1137f04661443241022ef422e1bf
s3fs <northfacebodybags@yahoo.com>
2020-11-26 00:27:01 +0500

    Intermediate commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Papa from 'papaparse'
 const App = () => {
   const [ data, setData ] = useState([])
   const [ obj, setNewObj ] = useState({})
+  const [ dateArr, setDateArr ] = useState([])
 
   const url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 
@@ -18,16 +19,26 @@ const App = () => {
         header: true 
       })
       setData(result.data)
-    })
-    .then(() => {
+
       const aData = data[0]
       console.log('aData', aData)
-      for (let [a, b] of Object.entries(aData)) {
-        console.log('a, b', a, b)
+      for (let [key, val] of Object.entries(aData)) {
+        const date = new Date()
+        let year = date.getFullYear()
+        year = year.toString()
+        year = year.slice(year.length - 2, year.length)
+
+        const dateCheck = new RegExp(`${date.getMonth() + 1}/${date.getDate()}/${year}`)
+        console.log('dateCheck', dateCheck)
+        if (dateCheck.test(key)) {
+          console.log(key, val)
+        }
+     
       }
     })
   }, [])
   
+
 
   return (
     <div>
