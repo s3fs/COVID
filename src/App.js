579eb2f0f2ad84678fc1ffc231356253192e70c8
s3fs <northfacebodybags@yahoo.com>
2020-11-25 18:18:20 +0500

    Add .csv to JSON via papaparse

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import './App.css';
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+import Papa from 'papaparse'
+
 
 const App = () => {
   const [ data, setData ] = useState([])
+  const [ obj, setNewObj ] = useState({})
+
+  const url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 
   useEffect(() => {
     axios
-    .get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-    .then(r => setData(r))
+    .get(url)
+    .then(r => {
+      const result = Papa.parse(r.data, { 
+        header: true 
+      })
+      setData(result.data)
+    })
+    .then(() => {
+      const aData = data[0]
+      console.log('aData', aData)
+      for (let [a, b] of Object.entries(aData)) {
+        console.log('a, b', a, b)
+      }
+    })
   }, [])
+  
 
   return (
     <div>
       <p>Hello world</p>
-      <p>{console.log(data)}</p>
+      <ul>{data.map(i => <li key={Math.random(999)}>{i['Country/Region']} {i['2/23/20']}</li>)}</ul>
     </div>
   )
 }
 
 export default App;
+
+/*
+axios
+    .get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+    .then(r => {
+      console.log('r', r)
+      setData(Papa.parse(r))
+    })
+*/
