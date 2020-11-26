92440a61999784bcfdc4734693ded471bd9e2697
s3fs <northfacebodybags@yahoo.com>
2020-11-26 18:58:47 +0500

    Refactor data fetching and managing to backend

--- src/App.js
+++ src/App.js
@@ -6,55 +6,20 @@ import Papa from 'papaparse'
 
 const App = () => {
   const [ data, setData ] = useState([])
-  const [ obj, setNewObj ] = useState({})
-  const [ dateArr, setDateArr ] = useState([])
 
-  const url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+  const url = 'https://cors-anywhere.herokuapp.com/https://warm-coast-62507.herokuapp.com/api/entries'
 
-  useEffect(() => {
-    axios
+  axios
     .get(url)
-    .then(r => {
-      const result = Papa.parse(r.data, { 
-        header: true 
-      })
-      setData(result.data)
-
-      const aData = data[0]
-      console.log('aData', aData)
-      for (let [key, val] of Object.entries(aData)) {
-        const date = new Date()
-        let year = date.getFullYear()
-        year = year.toString()
-        year = year.slice(year.length - 2, year.length)
-
-        const dateCheck = new RegExp(`${date.getMonth() + 1}/${date.getDate()}/${year}`)
-        console.log('dateCheck', dateCheck)
-        if (dateCheck.test(key)) {
-          console.log(key, val)
-        }
-     
-      }
-    })
-  }, [])
-  
+    .then(r => setData(r.data))
 
 
   return (
     <div>
       <p>Hello world</p>
-      <ul>{data.map(i => <li key={Math.random(999)}>{i['Country/Region']} {i['2/23/20']}</li>)}</ul>
+  <ul>{data.map(i => <li key={Math.random(999)}>{i['Province/State'] !== '' ? i['Province/State'] : null} {i['Country/Region']} {i['2/23/20']}</li>)}</ul>
     </div>
   )
 }
 
 export default App;
-
-/*
-axios
-    .get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-    .then(r => {
-      console.log('r', r)
-      setData(Papa.parse(r))
-    })
-*/
