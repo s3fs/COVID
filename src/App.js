6f1c82d66d300699199bccbf6b88827b7dd551d9
s3fs <northfacebodybags@yahoo.com>
2020-12-02 01:08:54 +0500

    adjustments for new backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import './App.css';
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import Papa from 'papaparse'
-
 
 const App = () => {
   const [ data, setData ] = useState([])
 
+
   const url = 'https://cors-anywhere.herokuapp.com/https://warm-coast-62507.herokuapp.com/api/entries'
 
   useEffect(() => {
+    console.log('useeff in use')
     axios
     .get(url)
     .then(r => setData(r.data))
   }, [])
 
-
   return (
     <div>
       <p>Hello world</p>
-  <ul>{data.map(i => <li key={Math.random(999)}>{i['Province/State'] !== '' ? i['Province/State'] : null} {i['Country/Region']} {i['2/23/20']}</li>)}</ul>
+      <ul>{data.map(i => <li key={Math.random(999)}>{i['province'] !== '' ? i['province'] : null} {i['country']} {i.cases.map(i => Object.entries(i).map(([ key, val ]) => <span>{key}, <b>{val}</b> </span>))}</li>)}</ul>
     </div>
   )
 }
